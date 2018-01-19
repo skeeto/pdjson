skeeto/pdjson

commit f8d6f13f42128812e322a60990ef5643244b33e9

json: allow custom allocator; improve stack semantics (resolve #3)
Allocating and freeing a bunch of tiny structures for a stack and
strings can be costly from a performance perspective (as can relying on
the sytem allocator). This change allows consumers to specify their own
allocation functions.

This change also removes unused / unnecessary fields from the "nesting"
struct, and renames it to `struct json_stack`. This struct is now
embedded in `json_stream`, and functions as an array. The stack is grown
four elements at a time (which is ~1 cache line on many modern
architectures) because magic numbers are hard. The stack is now treated
as an array with a size and a pointer to the stack top, instead of being
a linked list.

When exiting from a particular nesting, we do not free or realloc to a
smaller size. Most JSON streams I've encountered are not terribly deep,
and those that are tend to have a relatively constant depth. In other
words, deep JSON data structures likely have pessimistic allocation
patterns; it's best to just reuse the memory. At 16 bytes per nesting
level, you really have to try hard to make this actually be a waste of
memory.

I've also reordered the `json_stream` structure a bit to try to keep
related data close. On architectures with 64B cache lines, `json_stream`
now takes 4, but all the common mutable data is in 1 while the read-only
stuff is elsewhere.

json.c
@@ -15,51 +15,47 @@
                  __VA_ARGS__);                                    \
     }                                                             \
 
-struct nesting {
-    enum json_type type;
-    long count;
-    char meta;
-    struct nesting *next;
-};
+#define STACK_INC 4
 
 static enum json_type
 push(json_stream *json, enum json_type type)
 {
-    struct nesting *nesting = malloc(sizeof(struct nesting));
-    if (nesting == NULL) {
-        json_error(json, "%s", strerror(errno));
-        return JSON_ERROR;
+    json->stack_top++;
+
+    if (json->stack_top >= json->stack_size) {
+        struct json_stack *stack;
+        stack = json->alloc.realloc(json->stack,
+                (json->stack_size + STACK_INC) * sizeof(*json->stack));
+        if (stack == NULL) {
+            json_error(json, "%s", strerror(errno));
+            return JSON_ERROR;
+        }
+
+        json->stack_size += STACK_INC;
+        json->stack = stack;
     }
-    nesting->type = type;
-    nesting->count = 0;
-    nesting->meta = 0;
-    nesting->next = json->nesting;
-    json->nesting = nesting;
+
+    json->stack[json->stack_top].type = type;
+    json->stack[json->stack_top].count = 0;
+
     return type;
 }
 
 static enum json_type
 pop(json_stream *json, int c, enum json_type expected)
 {
-    struct nesting *nesting = json->nesting;
-    if (nesting == NULL || nesting->type != expected) {
+    if (json->stack == NULL || json->stack[json->stack_top].type != expected) {
         json_error(json, "unexpected byte, '%c'", c);
-        free(nesting);
+        json->alloc.free(json->stack);
         return JSON_ERROR;
     }
-    json->nesting = json->nesting->next;
-    free(nesting);
+    json->stack_top--;
     return expected == JSON_ARRAY ? JSON_ARRAY_END : JSON_OBJECT_END;
 }
 
 static void pop_all(json_stream *json)
 {
-    struct nesting *n = json->nesting;
-    while (n) {
-        struct nesting *current = n;
-        n = n->next;
-        free(current);
-    }
+    json->alloc.free(json->stack);
 }
 
 static int buffer_peek(struct json_source *source)
@@ -97,11 +93,19 @@ static void init(json_stream *json)
     json->errmsg[0] = '\0';
     json->ntokens = 0;
     json->next = 0;
-    json->nesting = NULL;
+
+    json->stack = NULL;
+    json->stack_top = -1;
+    json->stack_size = 0;
+
     json->data.string = NULL;
     json->data.string_size = 0;
     json->data.string_fill = 0;
     json->source.position = 0;
+
+    json->alloc.malloc = malloc;
+    json->alloc.realloc = realloc;
+    json->alloc.free = free;
 }
 
 static enum json_type
@@ -117,7 +121,7 @@ static int pushchar(json_stream *json, int c)
 {
     if (json->data.string_fill == json->data.string_size) {
         size_t size = json->data.string_size * 2;
-        char *buffer = realloc(json->data.string, size);
+        char *buffer = json->alloc.realloc(json->data.string, size);
         if (buffer == NULL) {
             json_error(json, "%s", strerror(errno));
             return -1;
@@ -135,7 +139,7 @@ static int init_string(json_stream *json)
     json->data.string_fill = 0;
     if (json->data.string == NULL) {
         json->data.string_size = 1024;
-        json->data.string = malloc(json->data.string_size);
+        json->data.string = json->alloc.malloc(json->data.string_size);
         if (json->data.string == NULL) {
             json_error(json, "%s", strerror(errno));
             return -1;
@@ -458,17 +462,17 @@ enum json_type json_next(json_stream *json)
         json->next = 0;
         return next;
     }
-    if (json->ntokens > 0 && json->nesting == NULL)
+    if (json->ntokens > 0 && json->stack_top == -1)
         return JSON_DONE;
     int c = next(json);
-    if (json->nesting == NULL)
+    if (json->stack == NULL)
         return read_value(json, c);
-    if (json->nesting->type == JSON_ARRAY) {
-        if (json->nesting->count == 0) {
-            json->nesting->count++;
+    if (json->stack[json->stack_top].type == JSON_ARRAY) {
+        if (json->stack[json->stack_top].count == 0) {
+            json->stack[json->stack_top].count++;
             return read_value(json, c);
         } else if (c == ',') {
-            json->nesting->count++;
+            json->stack[json->stack_top].count++;
             return read_value(json, next(json));
         } else if (c == ']') {
             return pop(json, c, JSON_ARRAY);
@@ -476,18 +480,18 @@ enum json_type json_next(json_stream *json)
             json_error(json, "unexpected byte, '%c'", c);
             return JSON_ERROR;
         }
-    } else if (json->nesting->type == JSON_OBJECT) {
-        if (json->nesting->count == 0) {
+    } else if (json->stack[json->stack_top].type == JSON_OBJECT) {
+        if (json->stack[json->stack_top].count == 0) {
             /* No property value pairs yet. */
             enum json_type value = read_value(json, c);
             if (value != JSON_STRING) {
                 json_error(json, "%s", "expected property name or '}'");
                 return JSON_ERROR;
             } else {
-                json->nesting->count++;
+                json->stack[json->stack_top].count++;
                 return value;
             }
-        } else if ((json->nesting->count % 2) == 0) {
+        } else if ((json->stack[json->stack_top].count % 2) == 0) {
             /* Expecting comma followed by property name. */
             if (c != ',' && c != '}') {
                 json_error(json, "%s", "expected ',' or '}'");
@@ -500,17 +504,17 @@ enum json_type json_next(json_stream *json)
                     json_error(json, "%s", "expected property name");
                     return JSON_ERROR;
                 } else {
-                    json->nesting->count++;
+                    json->stack[json->stack_top].count++;
                     return value;
                 }
             }
-        } else if ((json->nesting->count % 2) == 1) {
+        } else if ((json->stack[json->stack_top].count % 2) == 1) {
             /* Expecting colon followed by value. */
             if (c != ':') {
                 json_error(json, "%s", "expected ':' after property name");
                 return JSON_ERROR;
             } else {
-                json->nesting->count++;
+                json->stack[json->stack_top].count++;
                 return read_value(json, next(json));
             }
         }
@@ -560,10 +564,7 @@ size_t json_get_position(json_stream *json)
 
 size_t json_get_depth(json_stream *json)
 {
-    size_t depth = 0;
-    for (struct nesting *n = json->nesting; n; n = n->next)
-        depth++;
-    return depth;
+    return json->stack_top + 1;
 }
 
 void json_open_buffer(json_stream *json, const void *buffer, size_t size)
@@ -588,8 +589,13 @@ void json_open_stream(json_stream *json, FILE * stream)
     json->source.source.stream.stream = stream;
 }
 
+void json_set_allocator(json_stream *json, json_allocator *a)
+{
+    json->alloc = *a;
+}
+
 void json_close(json_stream *json)
 {
     pop_all(json);
-    free(json->data.string);
+    json->alloc.free(json->data.string);
 }

json.h
@@ -9,13 +9,21 @@ enum json_type {
     JSON_STRING, JSON_NUMBER, JSON_TRUE, JSON_FALSE, JSON_NULL
 };
 
+struct json_allocator {
+    void *(*malloc)(size_t);
+    void *(*realloc)(void *, size_t);
+    void (*free)(void *);
+};
+
 #include "json_private.h"
 
 typedef struct json_stream json_stream;
+typedef struct json_allocator json_allocator;
 
 void json_open_buffer(json_stream *json, const void *buffer, size_t size);
 void json_open_string(json_stream *json, const char *string);
-void json_open_stream(json_stream *json, FILE * stream);
+void json_open_stream(json_stream *json, FILE *stream);
+void json_set_allocator(json_stream *json, json_allocator *a);
 void json_close(json_stream *json);
 
 enum json_type json_next(json_stream *json);

json_private.h
@@ -18,18 +18,31 @@ struct json_source {
     } source;
 };
 
+struct json_stack {
+    enum json_type type;
+    long count;
+};
+
 struct json_stream {
     size_t lineno;
-    int error;
-    char errmsg[128];
+
+    struct json_stack *stack;
+    int_least32_t stack_top;
+    int_least32_t stack_size;
     enum json_type next;
-    struct nesting *nesting;
+    int error;
+
     struct {
         char *string;
-        size_t string_fill, string_size;
+        size_t string_fill;
+        size_t string_size;
     } data;
+
     size_t ntokens;
+
     struct json_source source;
+    struct json_allocator alloc;
+    char errmsg[128];
 };
 
 #endif

commit 921122e60c7813d29200296a121cc46470147138

Fix use-after-free and double free (#3, fixes #10)
f8d6f13f introduced a use-after-free and double free bugs when a stream
is reset. The stack is freed, but the old pointer is left in place and
re-used after passing a check against NULL.

Rather than free the stack on a reset, stack_top is reset to -1 (empty).
There was a check for an empty stack by comparing the pointer to NULL.
With the stack being re-used, the appropriate way to check for an empty
stack is to look at stack_top.

json.c
@@ -58,16 +58,10 @@ pop(json_stream *json, int c, enum json_type expected)
 {
     if (json->stack == NULL || json->stack[json->stack_top].type != expected) {
         json_error(json, "unexpected byte, '%c'", c);
-        json->alloc.free(json->stack);
         return JSON_ERROR;
     }
     json->stack_top--;
     return expected == JSON_ARRAY ? JSON_ARRAY_END : JSON_OBJECT_END;
-}
-
-static void pop_all(json_stream *json)
-{
-    json->alloc.free(json->stack);
 }
 
 static int buffer_peek(struct json_source *source)
@@ -669,7 +663,7 @@ enum json_type json_next(json_stream *json)
         return JSON_DONE;
     }
     int c = next(json);
-    if (json->stack == NULL)
+    if (json->stack_top == (size_t)-1)
         return read_value(json, c);
     if (json->stack[json->stack_top].type == JSON_ARRAY) {
         if (json->stack[json->stack_top].count == 0) {
@@ -736,7 +730,7 @@ enum json_type json_next(json_stream *json)
 
 void json_reset(json_stream *json)
 {
-    pop_all(json);
+    json->stack_top = -1;
     json->ntokens = 0;
     json->error = 0;
     json->errmsg[0] = '\0';
@@ -832,6 +826,6 @@ void json_set_streaming(json_stream *json, bool streaming)
 
 void json_close(json_stream *json)
 {
-    pop_all(json);
+    json->alloc.free(json->stack);
     json->alloc.free(json->data.string);
 }